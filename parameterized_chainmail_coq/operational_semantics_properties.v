8ec6a76aa8c869b8f7b00985ee2826a7323720eb
Julian Mackay <mackayjulian@gmail.com>
2020-11-11 08:39:25 +1300

    quick changes to boundary proofs

--- parameterized_chainmail_coq/operational_semantics_properties.v
+++ parameterized_chainmail_coq/operational_semantics_properties.v
@@ -121,6 +121,47 @@ Module OperationalSemanticsProperties(L : LanguageDef).
       eauto.
   Qed.
 
+  Lemma internal_reductions_preserves_internal :
+    forall M1 M2 σ1 σ2, M1 ⦂ M2 ⦿ σ1 ⤳… σ2 ->
+                   forall α, is_internal M1 M2 σ1 α ->
+                        is_internal M1 M2 σ2 α.
+  Proof.
+    intros M1 M2 σ1 σ2 Hred;
+      induction Hred;
+      intros;
+      eapply reduction_preserves_internal; eauto.
+  Qed.
+
+  Lemma pair_reduction_preserves_internal :
+    forall M1 M2 σ1 σ2, M1 ⦂ M2 ⦿ σ1 ⤳ σ2 ->
+                   forall α, is_internal M1 M2 σ1 α ->
+                        is_internal M1 M2 σ2 α.
+  Proof.
+    intros M1 M2 σ1 σ2 Hred;
+      induction Hred;
+      intros;
+      eapply reduction_preserves_internal; eauto.
+    eapply internal_reductions_preserves_internal; eauto.
+  Qed.
+
+  Lemma pair_reductions_preserves_internal :
+    forall M1 M2 σ1 σ2, M1 ⦂ M2 ⦿ σ1 ⤳⋆ σ2 ->
+                   forall α, is_internal M1 M2 σ1 α ->
+                        is_internal M1 M2 σ2 α.
+  Proof.
+    intros M1 M2 σ1 σ2 Hred;
+      induction Hred;
+      intros.
+
+    - eapply pair_reduction_preserves_internal; eauto.
+
+    - match goal with
+      | [H : forall _, _ |- _ ] =>
+        apply H
+      end.
+      eapply pair_reduction_preserves_internal; eauto.
+  Qed.
+
   Lemma reduction_preserves_external :
     forall M σ1 σ2, M ∙ σ1 ⤳ σ2 ->
                forall M1 M2 α, M1 ⋄ M2 ≜ M ->
@@ -144,6 +185,47 @@ Module OperationalSemanticsProperties(L : LanguageDef).
       auto.
     erewrite <- reduction_preserves_class;
       eauto.
+  Qed.
+
+  Lemma internal_reductions_preserves_external :
+    forall M1 M2 σ1 σ2, M1 ⦂ M2 ⦿ σ1 ⤳… σ2 ->
+                   forall α, is_external M1 M2 σ1 α ->
+                        is_external M1 M2 σ2 α.
+  Proof.
+    intros M1 M2 σ1 σ2 Hred;
+      induction Hred;
+      intros;
+      eapply reduction_preserves_external; eauto.
+  Qed.
+
+  Lemma pair_reduction_preserves_external :
+    forall M1 M2 σ1 σ2, M1 ⦂ M2 ⦿ σ1 ⤳ σ2 ->
+                   forall α, is_external M1 M2 σ1 α ->
+                        is_external M1 M2 σ2 α.
+  Proof.
+    intros M1 M2 σ1 σ2 Hred;
+      induction Hred;
+      intros;
+      eapply reduction_preserves_external; eauto.
+    eapply internal_reductions_preserves_external; eauto.
+  Qed.
+
+  Lemma pair_reductions_preserves_external :
+    forall M1 M2 σ1 σ2, M1 ⦂ M2 ⦿ σ1 ⤳⋆ σ2 ->
+                   forall α, is_external M1 M2 σ1 α ->
+                        is_external M1 M2 σ2 α.
+  Proof.
+    intros M1 M2 σ1 σ2 Hred;
+      induction Hred;
+      intros.
+
+    - eapply pair_reduction_preserves_external; eauto.
+
+    - match goal with
+      | [H : forall _, _ |- _ ] =>
+        apply H
+      end.
+      eapply pair_reduction_preserves_external; eauto.
   Qed.
 
   Lemma internal_is_not_external :
@@ -1085,6 +1167,122 @@ Module OperationalSemanticsProperties(L : LanguageDef).
 
   Qed.
 
+  Lemma cons_forall_in :
+    forall {A : Type}(l : list A)(a : A) P,
+      forall_in (a :: l) P ->
+      P a /\ forall_in l P.
+  Proof.
+    intros A l;
+      induction l;
+      intros;
+      unfold forall_in in *;
+      intros;
+      crush.
+  Qed.
+
+  Lemma forall_in_cons :
+    forall {A : Type}(l : list A)(a : A) (P : A -> Prop),
+      P a ->
+      forall_in l P ->
+      forall_in (a :: l) P.
+  Proof.
+    intros A l;
+      induction l;
+      intros;
+      unfold forall_in in *;
+      intros;
+      crush.
+  Qed.
+
+  Ltac forall_in_auto :=
+    match goal with
+    | [|- forall_in (_ :: _) _] =>
+      apply forall_in_cons
+    | [H : forall_in (_ :: _) _ |- _] =>
+      apply cons_forall_in in H;
+      andDestruct
+    end.
+
+  Lemma external_self_is_external :
+    forall M1 M2 χ self lcl c ψ, external_self M1 M2 (χ, frm self lcl c :: ψ) ->
+                            is_external M1 M2 (χ, frm self lcl c :: ψ) self.
+  Proof.
+    intros.
+    unfold external_self in *;
+      unfold_existentials;
+      auto.
+  Qed.
+
+  Lemma internal_self_is_internal :
+    forall M1 M2 χ self lcl c ψ, internal_self M1 M2 (χ, frm self lcl c :: ψ) ->
+                            is_internal M1 M2 (χ, frm self lcl c :: ψ) self.
+  Proof.
+    intros.
+    unfold internal_self in *;
+      unfold_existentials;
+      auto.
+  Qed.
+
+  Ltac external_internal_auto :=
+    match goal with
+    | [H : ?M1 ⦂ ?M2 ⦿ ?σa ⤳… ?σb |- _ ] =>
+      notHyp (external_self M1 M2 σa);
+      let Ha := fresh in
+      assert (H' : external_self M1 M2 σa);
+      [apply internal_reductions_is_from_external_config with (σ2:=σb); auto|]
+    | [H : ?M1 ⦂ ?M2 ⦿ ?σa ⤳… ?σb |- _ ] =>
+      notHyp (internal_self M1 M2 σa);
+      let Ha := fresh in
+      assert (H' : internal_self M1 M2 σb);
+      [apply internal_reductions_is_internal with (σ1:=σa); auto|]
+
+    | [H : internal_self ?M1 ?M2 (?χ, frm ?self ?lcl ?c :: ?ψ) |- _ ] =>
+      notHyp (is_internal M1 M2 (χ, frm self lcl c :: ψ) self);
+      let H' := fresh in
+      assert (H' : is_internal M1 M2 (χ, frm self lcl c :: ψ) self);
+      [eapply internal_self_is_internal; eauto|]
+    | [H : external_self ?M1 ?M2 (?χ, frm ?self ?lcl ?c :: ?ψ) |- _ ] =>
+      notHyp (is_external M1 M2 (χ, frm self lcl c :: ψ) self);
+      let H' := fresh in
+      assert (H' : is_external M1 M2 (χ, frm self lcl c :: ψ) self);
+      [eapply external_self_is_external; eauto|]
+
+    | [Hred : ?M ∙ ?σ1 ⤳ ?σ2,
+              Hlink : ?M1 ⋄ ?M2 ≜ ?M,
+                      Ha : is_internal ?M1 ?M2 ?σ1 ?α |- _ ] =>
+      notHyp (is_internal M1 M2 σ2 α);
+      let H' := fresh in
+      assert (H' : is_internal M1 M2 σ2 α);
+      [eapply reduction_preserves_internal; eauto|]
+    | [Hred : ?M ∙ ?σ1 ⤳ ?σ2,
+              Hlink : ?M1 ⋄ ?M2 ≜ ?M,
+                      Ha : is_external ?M1 ?M2 ?σ1 ?α |- _ ] =>
+      notHyp (is_external M1 M2 σ2 α);
+      let H' := fresh in
+      assert (H' : is_external M1 M2 σ2 α);
+      [eapply reduction_preserves_external; eauto|]
+
+    | [Hred : ?M1 ⦂ ?M2 ⦿ ?σ1 ⤳… ?σ2,
+              Ha : is_internal ?M1 ?M2 ?σ1 ?α |- _ ] =>
+      notHyp (is_internal M1 M2 σ2 α);
+      let H' := fresh in
+      assert (H' : is_internal M1 M2 σ2 α);
+      [eapply internal_reductions_preserves_internal; eauto|]
+    | [Hred : ?M1 ⦂ ?M2 ⦿ ?σ1 ⤳… ?σ2,
+              Ha : is_external ?M1 ?M2 ?σ1 ?α |- _ ] =>
+      notHyp (is_external M1 M2 σ2 α);
+      let H' := fresh in
+      assert (H' : is_external M1 M2 σ2 α);
+      [eapply internal_reductions_preserves_external; eauto|]
+
+    | [Ha : is_internal ?M1 ?M2 ?σ ?α,
+            Hb : is_external ?M1 ?M2 ?σ ?α |- _ ] =>
+      notHyp (~ is_external M1 M2 σ α);
+      apply internal_is_not_external in Ha;
+      contradiction Ha; auto
+
+    end.
+
   Lemma internal_reduction_is_call_or_rtrn :
     forall M1 M2 σ1 σ2, M1 ⦂ M2 ⦿ σ1 ⤳… σ2 ->
                    (exists χ1 self lcl x α m args b ψ1
@@ -1237,29 +1435,57 @@ Module OperationalSemanticsProperties(L : LanguageDef).
         | [H : _ ∙ _ ⤳ _ |- _ ] =>
           inversion H;
             subst
-        end.
+        end;
+          try solve [match goal with
+                     | [H : _ ∙ _ ⤳ (_, ?ϕ :: ?ψ ++ _ :: _) |- _ ] =>
+                       exists (ϕ :: ψ)
+                     end;
+                     repeat split;
+                     auto;
+                     repeat forall_in_auto;
+                     auto];
+          try solve [match goal with
+                     | [H : _ :: _ = _ ++ _ :: _ |- _ ] =>
+                       symmetry in H;
+                       destruct (app_cons_1 H);
+                       subst;
+                       simpl in *;
+                       repeat simpl_crush
+                     end;
+                     [repeat forall_in_auto;
+                      unfold internal_frame in *;
+                      simpl in *;
+                      repeat external_internal_auto
+                     |unfold_existentials;
+                      match goal with
+                      | [H : _ ∙ _ ⤳ (_, ?ϕ :: ?ψ ++ _ :: _) |- _ ] =>
+                        exists (ϕ :: ψ)
+                      end;
+                      repeat split;
+                      simpl;
+                      auto;
+                      try solve [crush];
+                      unfold internal_frame in *;
+                      repeat forall_in_auto;
+                      auto]].
 
-      + right.
-        match goal with
-        | [Hred : _ ∙ (_, ?ψ ++ _) ⤳ _ |-_ ] =>
-          let ϕ' := fresh "ϕ" in
-          let ψ' := fresh "ψ" in
-          destruct ψ as [|ϕ' ψ'];
+        * match goal with
+          | [H : _ ∙ _ ⤳ (_, ?ϕ1 :: ?ϕ2 :: ?ψ ++ _ :: _) |- _ ] =>
+            exists (ϕ1 :: ϕ2 :: ψ)
+          end;
+            repeat split;
             simpl in *;
-            [crush|]
-        end.
+            auto;
+            repeat forall_in_auto;
+            auto.
+          unfold internal_frame;
+            simpl.
+          unfold internal_self in *;
+            unfold_existentials;
+            auto.
 
-        match goal with
-        | [|- exists _ _ _ _ _ _ _, (?χ, ?ϕ :: ?ψ1 ++ ?ψ2) = _ /\ ?σ = _ /\ _ ] =>
-          match goal with
-          | [_ : context[rtrns _ ?v] |- _] =>
-            exists χ, v, ϕ, ψ2, ψ1;
-              let χ' := fresh "χ" in
-              let ψ' := fresh "ψ" in
-              destruct σ as [χ' ψ'];
-                exists χ'
-          end
-        end.
+        *
+
 
   Qed.
 
