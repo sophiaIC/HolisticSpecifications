a8d7019be9cb49e5842f8b27decec6c04dc0b322
Julian <mackayjulian@gmail.com>
2021-11-30 12:23:56 +1300

    changed crowdsale example to work in TooL

--- popl2022/verx_encoding.sol
+++ popl2022/verx_encoding.sol
@@ -1,64 +1,84 @@
 contract Crowdsale {
     Escrow escrow;
+    EscrowPublic escrow_p;
     uint256 closeTime;
     uint256 raised = 0;
     uint256 goal = 10000 * 10**18;
 
     function constructor() {
-        escrow = new Escrow(0x1234);
+        escrow = new Escrow(this, 0x1234);
+        escrow_p = new EscrowPublic(escrow);
         closeTime = now + 20 days;
     }
 
-    function invest() payable {
-        require (raised < goal);
-        require (now <= closeTime);
-        escrow.deposit.value(msg.value)(msg.sender);
-        raised += msg.value;
+    function invest(address sender, uint256 value) payable {
+        if(raised < goal && now <= closeTime){
+            escrow.deposit(sender, value);
+            raised += msg.value;
+        }
     }
 
     function close() {
-        require (now > closeTime || raised >= goal);
-        if (raised >= goal) {
-            escrow.close();
-        } else {
-            escrow.refund();
+        if (now > closeTime || raised >= goal){
+            if (raised >= goal) {
+                escrow.close();
+            } else {
+                escrow.refund();
+            }
         }
     }
 }
 
+/*
+ * The private Escrow class
+ */
 contract Escrow {
     address owner, beneficiary;
     mapping (address => uint256) deposits;
     enum State (OPEN, SUCCESS, WITHDRAWN, REFUNDABLE, REFUNDED);
     State state = OPEN;
 
-    constructor (address b) {
-        owner = msg.sender;
+    constructor (address o, address b) {
+        owner = o;
         beneficiary = b;
     }
-    modifier onlyOwner {
-        require (msg.sender == owner);
-    }
-    modifier onlyThis {
-        require (msg.sender == this);
-    }
-    function close() onlyOwner {state = SUCCESS;}
-    function setRefundable() onlyOwner {state = REFUNDABLE;}
-    function setRefunded() onlyThis {state = REFUNDED;}
-    function setWithdrawn() onlyThis {state = WITHDRAWN;}
-    function deposit(address p) onlyOwner payable {
-        deposits[p] = deposits[p] + msg.value;
+
+    function close() {state = SUCCESS;}
+    function setRefundable() {state = REFUNDABLE;}
+    function setRefunded() {state = REFUNDED;}
+    function setWithdrawn() {state = WITHDRAWN;}
+    function deposit(address p, uint256 value) {
+        deposits[p] = deposits[p] + value;
     }
     function withdraw(){
-        require(state == SUCCESS);
-        this.setWithdrawn();
-        beneficiary.transfer(this.balance);
+        if(state == SUCCESS){
+            this.setWithdrawn();
+            beneficiary.transfer(this.balance);
+        }
     }
     function claimRefund(address p){
-        require(state == REFUNDABLE);
-        uint256 amount = this.deposits[p];
-        this.setRefunded();
-        this.deposits[p] = 0;
-        p.call.value(amount)();
+        if(state == REFUNDABLE){
+            uint256 amount = this.deposits[p];
+            this.setRefunded();
+            this.deposits[p] = 0;
+            p.call.value(amount)();
+        }
+    }
+}
+
+/*
+ * The public interface of Escrow
+ * the withdraw and refund methods of the escrow are exposed
+ */
+contract EscrowPublic{
+    Escrow escrow;
+    constructor (Escrow e){
+        escrow = e;
+    }
+    function withdraw(){
+        this.escrow.withdraw();
+    }
+    function claimRefund(address p){
+        this.escrow.claimRefund(p)
     }
 }
