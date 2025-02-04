3dae5e28082fcda52f481de2e72fec8003adbd04
Julian Mackay <mackayjulian@gmail.com>
2025-02-04 21:03:27 +1300

    split on proof/lemmas/assumptions

---
+++ ambient_capabilities_coq_model/assumptions.v
@@ -0,0 +1,245 @@
+Require Export Arith.
+Require Import List.
+Require Import Bool.
+Require Import String.
+
+Require Import CpdtTactics.
+Require Import common.
+Require Import language_def.
+Require Import subst.
+Require Import assert.
+Require Export operational_semantics.
+Require Import assert_theory.
+Require Import hoare.
+Require Import spec.
+Require Import shop.
+
+Require Export Coq.Numbers.BinNums.
+Require Export ZArith.
+
+Module Assumptions.
+
+  Import LanguageDefinition.
+  Import OperationalSemantics.
+  Import Assert.
+  Import SubstDefn.
+  Import AssertTheory.
+  Import Hoare.
+  Import SpecSatisfaction.
+  Import Shop.
+
+  Open Scope hoare_scope.
+  Open Scope assert_scope.
+
+  Lemma apply_entails :
+    forall M σ A1 A2, M ⊢ A1 ⊆ A2 ->
+                 sat M σ A1 ->
+                 sat M σ A2.
+  Proof.
+  Admitted.
+
+   Lemma entails_fld_type :
+    forall M C f T, typeOf_f M C f = Some T ->
+               forall e, M ⊢ (a_ (e_typ e (t_cls C))) ⊆
+                           (a_ (e_typ (e_fld e f) T)).
+  Proof.
+
+  Admitted.
+
+  Lemma sat_excluded_middle :
+    forall M σ A, sat M σ (A ∨ ¬ A).
+  Proof.
+  Admitted.
+
+  Lemma sat_neg_is_not_sat :
+    forall M σ A, sat M σ (¬ A) ->
+             ~ sat M σ A.
+  Proof.
+    intros.
+  Admitted.
+
+  Lemma entails_excluded_middle :
+    forall M A, M ⊢ a_true ⊆ (A ∨ ¬ A).
+  Admitted.
+
+  Lemma or_and_dist1 :
+    forall M A1 A2 A, M ⊢ (A1 ∨ A2) ∧ A ⊆ (A1 ∧ A) ∨ (A2 ∧ A).
+  Admitted.
+
+  Lemma or_and_dist2 :
+    forall M A1 A2 A, M ⊢ (A1 ∧ A) ∨ (A2 ∧ A) ⊆ (A1 ∨ A2) ∧ A.
+  Admitted.
+
+  Lemma entails_assoc1 :
+    forall M A1 A2 A3, M ⊢ ((A1 ∧ A2) ∧ A3) ⊆ (A1 ∧ (A2 ∧ A3)). Admitted.
+
+  Lemma entails_assoc2 :
+    forall M A1 A2 A3, M ⊢ (A1 ∧ (A2 ∧ A3)) ⊆ ((A1 ∧ A2) ∧ A3). Admitted.
+
+  Lemma entails_trans :
+    forall M A1 A2 A3, M ⊢ A1 ⊆ A2 ->
+                  M ⊢ A2 ⊆ A3 ->
+                  M ⊢ A1 ⊆ A3.
+  Admitted.
+
+  Lemma hq_conj_assoc1_pre :
+    forall M A1 A2 A3 A4 s A, M ⊢ ⦃ A1 ∧ (A2 ∧ A3) ⦄ s ⦃ A4 ⦄ || ⦃ A ⦄ ->
+                         M ⊢ ⦃ (A1 ∧ A2) ∧ A3 ⦄ s ⦃ A4 ⦄ || ⦃ A ⦄.
+  Proof.
+  Admitted.
+
+  Lemma hq_conj_assoc2_post :
+    forall M A1 A2 A3 A4 s A, M ⊢ ⦃ A1 ⦄ s ⦃ A2 ∧ (A3 ∧ A4) ⦄ || ⦃ A ⦄ ->
+                         M ⊢ ⦃ A1 ⦄ s ⦃ (A2 ∧ A3) ∧ A4 ⦄ || ⦃ A ⦄.
+  Proof.
+  Admitted.
+
+  Lemma hq_conj_assoc2_pre :
+    forall M A1 A2 A3 A4 s A, M ⊢ ⦃ (A1 ∧ A2) ∧ A3 ⦄ s ⦃ A4 ⦄ || ⦃ A ⦄ ->
+                         M ⊢ ⦃ A1 ∧ (A2 ∧ A3) ⦄ s ⦃ A4 ⦄ || ⦃ A ⦄.
+  Proof.
+  Admitted.
+
+  Lemma hq_conj_assoc1_post :
+    forall M A1 A2 A3 A4 s A, M ⊢ ⦃ A1 ⦄ s ⦃ (A2 ∧ A3) ∧ A4 ⦄ || ⦃ A ⦄ ->
+                         M ⊢ ⦃ A1 ⦄ s ⦃ A2 ∧ (A3 ∧ A4) ⦄ || ⦃ A ⦄.
+  Proof.
+  Admitted.
+
+  Lemma rewrite_hoare_quad1 :
+    forall M A A1, M ⊢ A1 ⊆ A ->
+              forall A2 A3 s, M ⊢ ⦃ A ⦄ s ⦃ A2 ⦄ || ⦃ A3 ⦄ ->
+                         M ⊢ ⦃ A1 ⦄ s ⦃ A2 ⦄ || ⦃ A3 ⦄.
+  Proof.
+  Admitted.
+
+  Lemma rewrite_hoare_quad2 :
+    forall M A A2, M ⊢ A ⊆ A2 ->
+              forall A1 A3 s, M ⊢ ⦃ A1 ⦄ s ⦃ A ⦄ || ⦃ A3 ⦄ ->
+                         M ⊢ ⦃ A1 ⦄ s ⦃ A2 ⦄ || ⦃ A3 ⦄.
+  Proof.
+  Admitted.
+
+  Lemma conj_strengthen_entails :
+    forall M A1 A2 A, M ⊢ A1 ⊆ A2 ->
+                 M ⊢ (A1 ∧ A) ⊆ A2.
+  Proof.
+  Admitted.
+
+  Lemma entails_prt_intl :
+    forall M x y C, M ⊢ ((a_ e_typ y (t_cls C)) ∧ ¬ a_extl y) ⊆ a_prt_frm x y.
+  Proof.
+  Admitted.
+
+  Lemma entails_intl :
+    forall M e C, C ∈ (snd M) ->
+             M ⊢ (a_ (e_typ e (t_cls C))) ⊆ (¬ a_extl e).
+  Admitted.
+
+  Lemma entails_different_type_neq :
+    forall M e1 e2 T1 T2, T1 <> T2 ->
+                     M ⊢ ((a_ e_typ e1 T1) ∧ (a_ e_typ e2 T2)) ⊆
+                       ¬ (a_ (e_eq e1 e2)).
+  Proof.
+    intros.
+  Admitted.
+
+  Lemma exp_neq_different_type :
+    forall M e1 e2 T1 T2,
+      T1 <> T2 ->
+      M ⊢ (a_ e_typ e1 T1) ∧ (a_ e_typ e2 T2) ⊆
+                               (¬ a_ (e_eq e1 e2)).
+  Proof.
+  Admitted.
+
+  Lemma exp_eq_same_type :
+    forall M e1 e2 T,
+      M ⊢ (a_ e_typ e1 T) ∧ (a_ e_eq e1 e2) ⊆ (a_ e_typ e2 T).
+  Proof.
+  Admitted.
+
+  Lemma entails_eq_trans :
+    forall M e1 e2 e3, M ⊢ a_ (e_eq e1 e2) ∧ a_ (e_eq e2 e3) ⊆ a_ (e_eq e1 e3).
+  Admitted.
+
+  Lemma entails_eq_fld :
+    forall M e1 e2 f, M ⊢ a_ (e_eq e1 e2) ⊆ a_ (e_eq (e1 ∙ f) (e2 ∙ f)).
+  Admitted.
+
+  Lemma entails_eq_not_prt_frm :
+    forall M e1 e2, M ⊢ a_ (e1 ⩵ e2) ⊆ ¬ a_prt_frm e1 e2.
+  Admitted.
+
+  Lemma entails_prt_eq :
+    forall M e1 e2, M ⊢ a_ (e_eq e1 e2) ∧ a_prt e1 ⊆ a_prt e2.
+  Admitted.
+
+  Lemma entails_prt_null :
+    forall M, M ⊢ a_prt e_null ⊆ a_false.
+  Admitted.
+
+  Lemma hoare_false :
+    forall M s A, M ⊢ ⦃ a_false ⦄ s ⦃ A ⦄.
+  Admitted.
+
+  Lemma neg_absurd :
+    forall M A, M ⊢ A ∧ ¬ A ⊆ a_false.
+  Admitted.
+
+  Parameter hoare_UL_write_different_field :
+    forall M x f y f' e z,
+      f <> f' ->
+      hoare_base M (a_ (e_eq (e_fld (e_ x) f) (e_ z)))
+                 (s_write y f' e)
+                 (a_ (e_eq (e_fld (e_ x) f) (e_ z))).
+
+  Parameter hoare_UL_write_different_field_lt :
+    forall M x f y f' e z,
+      f <> f' ->
+      hoare_base M (a_ (e_lt (e_ z) (e_fld (e_ x) f)))
+        (s_write y f' e)
+        (a_ (e_lt (e_ z) (e_fld (e_ x) f))).
+
+  Parameter hoare_UL_write_different_object :
+    forall M x f y f' e z,
+      hoare_base M ((¬ (a_ (e_eq (e_ x) (e_ y)))) ∧
+                      (a_ (e_eq (e_fld (e_ x) f) (e_ z))))
+                 (s_write y f' e)
+                 (a_ (e_eq (e_fld (e_ x) f) (e_ z))).
+
+  Parameter hoare_UL_addition_lt :
+    forall M x f y z f' i,
+      hoare_base M (a_ (e_lt (e_ y) (e_fld (e_ x) f)))
+        (s_write z f' (e_plus ((e_ z) ∙ f') i))
+        (a_ (e_lt (e_ y) (e_fld (e_ x) f))).
+
+  Parameter UL_post_true :
+    forall M A s, hoare_base M A s (a_true).
+
+  Lemma hoare_ul_write_to_different_object_lt :
+    forall M x y z f1 f2 e,
+      M ⊢ ⦃ (e_ x) ≠ (e_ y) ∧ a_ e_lt (e_ z) ((e_ x) ∙ f1) ⦄
+        s_write y f2 e
+        ⦃ a_ e_lt (e_ z) ((e_ x) ∙ f1) ⦄.
+  Admitted.
+
+  Lemma hoare_ul_write_to_different_object_eq :
+    forall M x y z f1 f2 e,
+      M ⊢ ⦃ (e_ x) ≠ (e_ y) ∧ a_ e_eq (e_ z) ((e_ x) ∙ f1) ⦄
+        s_write y f2 e
+        ⦃ a_ e_eq (e_ z) ((e_ x) ∙ f1) ⦄.
+  Admitted.
+
+  Parameter hoare_ul_return_bool_preserves_fld_lt :
+    forall M x y f, hoare_base M
+                 ((a_ e_typ (e_ result) t_bool) ∧ a_ (e_lt (e_ x) ((e_ y) ∙ f)))
+                 (ret e_false)
+                 (a_ (e_lt (e_ x) ((e_ y) ∙ f))).
+
+  Parameter hoare_ul_return_bool_preserves_fld_eq :
+    forall M x y f, hoare_base M
+                      ((a_ e_typ (e_ result) t_bool) ∧ a_ (e_eq ((e_ x) ∙ f) (e_ y)))
+                      (ret e_false)
+                      (a_ (e_eq ((e_ x) ∙ f) (e_ y))).
+
+End Assumptions.
---
+++ ambient_capabilities_coq_model/shop.v
@@ -0,0 +1,342 @@
+Require Export Arith.
+Require Import List.
+Require Import Bool.
+Require Import String.
+
+Require Import CpdtTactics.
+Require Import common.
+Require Import language_def.
+Require Import subst.
+Require Import assert.
+Require Export operational_semantics.
+Require Import assert_theory.
+Require Import hoare.
+Require Import spec.
+
+Require Export Coq.Numbers.BinNums.
+Require Export ZArith.
+
+Module Shop.
+
+  Import LanguageDefinition.
+  Import OperationalSemantics.
+  Import Assert.
+  Import SubstDefn.
+  Import AssertTheory.
+  Import Hoare.
+  Import SpecSatisfaction.
+
+  Open Scope hoare_scope.
+  Open Scope assert_scope.
+
+  (***
+
+      class Shop{
+            acc : Account
+            myItem : Item
+            client : external
+            public method buy(buyer : external, anItem : Item)
+                   int price = myItem.price
+                   int oldBalance = this.acc.balance
+                   buyer.pay(this.acc, price)
+                   if this.acc.balance == oldBalance + price
+                      buyer.send(myItem)
+                      // this.send(buyer, myItem)
+                   else
+                      buyer.tell("pay me!!!!")
+           private send(buyer : external, anItem : Item)
+      }
+
+      class Account {
+            balance : int
+            key : Key
+            public method transfer(to : Account, k : Key, amt : int)
+                   if this.key == k
+                      this.balance -= amt
+                      to.balance += amt
+            public method set(k : Key)
+                   if this.key == null
+                      this.key = k
+      }
+
+      class Key {}
+
+      class Item {
+            price : int
+      }
+
+e ::=
+e.f
+x
+bool
+int
+if e then e else e
+e - e
+e + e
+e.g(e)
+e : C
+
+
+   ***)
+
+  (*)Parameter s_minus (x y : var)(i : Z) : stmt.
+
+  Parameter s_plus (x y : var)(i : Z) : stmt.*)
+
+  (*Parameter plus_hoare (M : module)(x y : var)(i : Z) :=
+   M ⊢ {} x*)
+
+  Definition Shop := c_id 1.
+
+  Definition Account := c_id 2.
+
+  Definition Key := c_id 3.
+
+  Definition Item := c_id 4.
+
+  Definition acc := f_id 0.
+
+  Definition myItem := f_id 1.
+
+  Definition client := f_id 2.
+
+  Definition balance := f_id 3.
+
+  Definition key := f_id 4.
+
+  Definition price := f_id 5.
+
+  Definition ShopFlds :=
+    ⟦ acc ↦ t_cls Account ⟧
+      ⟦ myItem ↦ t_cls Item ⟧
+      ⟦ client ↦ t_ext ⟧ ∅.
+
+  Definition AccountFlds :=
+    ⟦ balance ↦ t_int ⟧
+      ⟦ key ↦ t_cls Key ⟧ ∅.
+
+  Definition ItemFlds :=
+    ⟦ price ↦ t_int ⟧ ∅.
+
+  Definition buy := mth_id 0.
+
+  Definition buyer := v_prog 2.
+
+  Definition item := v_prog 3.
+
+  Definition itemPrice := v_prog 4.
+
+  Definition oldBalance := v_prog 5.
+
+  Definition thisAcc := v_prog 6.
+
+  Definition tmp := v_prog 7.
+
+  Definition pay := mth_id 1.
+
+  Definition send := mth_id 2.
+
+  Definition tell := mth_id 3.
+
+  Definition a := v_spec 11.
+
+  Definition b := v_spec 12.
+
+  (*
+    itemPrice = item.price
+    thisAcc = this.acc
+    oldBalance = thisAcc.balance
+    tmp = buyer.pay(thisAcc, itemPrice)
+    if this.acc.balance == oldBalance + itemPrice
+       tmp = buyer.send(item)
+    else
+       tmp = buyer.buyFailed()
+    return tmp
+   *)
+
+  Definition buyBody := (s_read itemPrice (e_fld (e_ item) price)) ;;
+                        (s_read thisAcc (e_fld (e_ this) acc) ;;
+                         (s_read oldBalance (e_fld (e_ thisAcc) balance) ;;
+                          (s_call tmp buyer pay (thisAcc :: itemPrice :: nil) ;;
+                           (s_if (e_eq
+                                    (e_fld (e_fld (e_ this) acc) balance)
+                                    (e_plus (e_ oldBalance) (e_ itemPrice)))
+                              (s_call tmp this send (buyer :: item :: nil))
+                              (s_call tmp buyer tell nil) ;;
+                            ret (e_false))))).
+
+  (* TODO: method specs on buy *)
+  Definition buyDef := meth nil
+                         public
+                         ((buyer, t_ext)::
+                            (item, t_cls Item) :: nil)
+                         buyBody
+                         t_bool.
+
+  Parameter sendBody : stmt.
+
+  Definition sendDef := meth ((a_ (e_typ (e_ a) (t_cls Account)) ∧
+                                 (a_prt ((e_ a) ∙ key)),
+                                a_prt ((e_ a) ∙ key),
+                                a_prt ((e_ a) ∙ key)) ::
+                                ((a_ (e_typ (e_ a) (t_cls Account))) ∧
+                                   (a_prt_frm ((e_ a) ∙ key) (e_ buyer)),
+                                  a_prt_frm ((e_ a) ∙ key) (e_ buyer),
+                                  a_prt ((e_ a) ∙ key)) ::
+                                (a_ (e_typ (e_ a) (t_cls Account)) ∧
+                                   (a_ (e_typ (e_ b) t_int) ∧
+                                      (a_prt ((e_ a) ∙ key) ∧ (a_ (e_lt (e_ b)((e_ a) ∙ balance))))),
+                                  a_prt ((e_ a) ∙ key) ∧ (a_ (e_lt (e_ b)((e_ a) ∙ balance))),
+                                  a_prt ((e_ a) ∙ key) ∧ (a_ (e_lt (e_ b)((e_ a) ∙ balance)))) ::
+                                nil)
+                          private
+                          ((buyer, t_ext) ::
+                             (item, t_cls Item) :: nil)
+                          sendBody
+                          t_bool.
+
+  Definition ShopMths := ⟦ buy ↦ buyDef ⟧ ⟦ send ↦ sendDef ⟧ ∅.
+
+  Definition ShopDef := clazz Shop
+                              ShopFlds
+                              empty
+                              ShopMths.
+
+  Definition to := v_prog 8.
+
+  Definition k := v_prog 9.
+
+  Definition amt := v_prog 10.
+
+  Definition transfer := mth_id 4.
+
+  Definition transferBody :=
+    s_if (e_eq (e_fld (e_ this) key) (e_ k))
+      (s_write this balance (e_minus (e_fld (e_ this) balance) (e_ amt)) ;; 
+       (s_write to balance (e_plus (e_fld (e_ to) balance) (e_ amt))))
+      (ret e_false);;
+    ret e_false.
+
+  Definition transferDef := meth nil
+                              public
+                              ((k, t_cls Key) ::
+                                 (to, t_cls Account) ::
+                                 (amt, t_int) :: nil)
+                              transferBody
+                              t_bool.
+
+  Definition setKey := mth_id 5.
+
+  Definition setKeyBody :=
+    s_if (e_eq (e_fld (e_ this) key) e_null)
+      (s_write this key (e_ k) ;;
+       ret e_false)
+      (ret e_false).
+
+  Definition setKeyDef := meth nil
+                            public
+                            ((k, t_cls Key) :: nil)
+                            setKeyBody
+                            t_bool.
+
+  Definition AccountMths := ⟦ transfer ↦ transferDef ⟧
+                              ⟦ setKey ↦ setKeyDef ⟧ ∅.
+
+  Definition AccountDef := clazz Account
+                             AccountFlds
+                             empty
+                             AccountMths.
+
+  Definition KeyDef := clazz Key
+                             empty
+                             empty
+                             empty.
+
+  Definition ItemDef := clazz Item
+                          (⟦ price ↦ t_int ⟧ ∅)
+                          empty
+                          empty.
+
+  (* Shop Specifications *)
+
+  Definition S2 := S_inv ((a, t_cls Account)::nil) (a_prt (e_fld (e_ a) key)).
+
+  Definition S3 := S_inv ((a, t_cls Account)::(b, t_int)::nil) (a_prt (e_fld (e_ a) key) ∧ a_ (e_lt (e_ b) (e_fld (e_ a) balance))). 
+
+  Definition Mgood : module := (S_and S2 S3,
+                                 ⟦ Shop ↦ ShopDef ⟧
+                                   ⟦ Account ↦ AccountDef ⟧
+                                   ⟦ Key ↦ KeyDef ⟧
+                                   ⟦ Item ↦ ItemDef ⟧ ∅).
+
+  Lemma destruct_Mgood :
+    forall C CDef, ⟦ C ↦ CDef ⟧_∈ snd Mgood ->
+              (C = Shop /\ CDef = ShopDef) \/
+                (C = Account /\ CDef = AccountDef) \/
+                (C = Key /\ CDef = KeyDef).
+  Proof.
+  Admitted.
+
+  Lemma destruct_shopMths :
+    forall m mDef, ⟦ m ↦ mDef ⟧_∈ c_meths ShopDef ->
+              (m = buy /\ mDef = buyDef) \/
+                (m = send /\ mDef = sendDef).
+  Proof.
+  Admitted.
+
+  Lemma destruct_accountMths :
+    forall m mDef, ⟦ m ↦ mDef ⟧_∈ c_meths AccountDef ->
+              (m = transfer /\ mDef = transferDef) \/
+                (m = setKey /\ mDef = setKeyDef).
+  Proof.
+  Admitted.
+
+(*)  Lemma destruct_keyMths :
+    forall m mDef, ⟦ m ↦ mDef ⟧_∈ c_meths KeyDef ->
+              mDef =
+              (m = transfer /\ mDef = transferDef) \/
+                (m = setKey /\ mDef = setKeyDef).
+  Proof.
+  Admitted.*)
+
+  Lemma keyHasTypeKey :
+    forall x, Mgood ⊢ (a_ e_typ x (t_cls Account)) ⊆
+           (a_ e_typ (e_fld x key) (t_cls Key)).
+  Proof.
+  Admitted.
+
+  Lemma balanceHasTypeInt :
+    forall x, Mgood ⊢ (a_ e_typ x (t_cls Account)) ⊆
+           (a_ e_typ (e_fld x balance) t_int).
+  Proof.
+  Admitted.
+
+  Lemma type_of_itemPrice :
+    typeOf_f Mgood Item price = Some t_int.
+  Proof.
+    auto.
+  Qed.
+
+  Lemma type_of_shopAcc :
+    typeOf_f Mgood Shop acc = Some (t_cls Account).
+  Proof.
+    auto.
+  Qed.
+
+  Lemma type_of_accountBalance :
+    typeOf_f Mgood Account balance = Some t_int.
+  Proof.
+    auto.
+  Qed.
+
+  #[global] Hint Resolve type_of_itemPrice type_of_shopAcc type_of_accountBalance : shop_db.
+
+  Ltac setup_shop :=
+    assert (HitemPrice : ⟦ price ↦ t_int ⟧_∈ typeOf_f Mgood Item);
+    [apply type_of_itemPrice|];
+    assert (HaccountBalance : ⟦ balance ↦ t_int ⟧_∈ typeOf_f Mgood Account);
+    [apply type_of_accountBalance|];
+    assert (HshopAccount : ⟦ acc ↦ t_cls Account ⟧_∈ typeOf_f Mgood Shop);
+    [apply type_of_shopAcc|].
+
+End Shop.
